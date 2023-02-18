273c56ee015bc050628d23e1e03eaac7004d1d19
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-02-18 00:28:01 -0700

    feat: add to_string for compilation unit

--- lib/ast.ml
+++ lib/ast.ml
@@ -18,22 +18,56 @@ type classDeclaration =
 
 type compilationUnit = TypeDecl of modifier list * classDeclaration
 
-let to_string_identifier (identifier : identifier) =
-  match identifier with Identifier name -> "Identifier(\"" ^ name ^ "\")"
+let rec tabs (num : int) = if num == 0 then "" else tabs (num - 1) ^ "  "
 
-let to_string_modifier (modifier : modifier) =
+let to_string_identifier (identifier : identifier) (depth : int) =
+  match identifier with
+  | Identifier name -> "(Identifier\n" ^ tabs (depth + 1) ^ "(\"" ^ name ^ "\"))"
+
+let to_string_modifier (modifier : modifier) (depth : int) =
   match modifier with
-  | Public -> "Public"
-  | Annotation identifer -> "Annotation(" ^ to_string_identifier identifer ^ ")"
+  | Public -> "(Public)"
+  | Annotation identifer ->
+      "(Annotation\n"
+      ^ tabs (depth + 1)
+      ^ to_string_identifier identifer (depth + 1)
+      ^ ")"
 
-let rec _to_string_modifiers (modifiers : modifier list) =
+let rec _to_string_modifiers (modifiers : modifier list) (depth : int) =
   match modifiers with
   | [] -> ""
-  | h :: [] -> to_string_modifier h
-  | h :: t -> to_string_modifier h ^ ", " ^ _to_string_modifiers t
+  | h :: [] -> tabs depth ^ to_string_modifier h depth
+  | h :: t ->
+      tabs depth ^ to_string_modifier h depth ^ ";\n"
+      ^ _to_string_modifiers t depth
 
-let to_string_modifiers (modifiers : modifier list) =
-  "[" ^ (_to_string_modifiers modifiers) ^ "]"
+let to_string_modifiers (modifiers : modifier list) (depth : int) =
+  "[\n" ^ _to_string_modifiers modifiers (depth + 1) ^ "\n" ^ tabs depth ^ "]"
+
+let to_string_class_body_decls (class_body_decls : classBodyDeclaration list) (depth : int) =
+  match class_body_decls with 
+    | [] -> tabs depth ^ "[]"
+    | _ -> "Not implemented with class body decls to string"
+
+let to_string_class_declaration (class_decl : classDeclaration) (depth : int) =
+  match class_decl with
+  | ClassDeclaration (identifer, class_body_decls) ->
+      tabs depth ^ "(ClassDeclaration\n"
+      ^ tabs (depth + 1)
+      ^ to_string_identifier identifer (depth + 1)
+      ^ ",\n"
+      ^ tabs (depth)
+      ^ to_string_class_body_decls class_body_decls (depth)
+      ^ "))"
+
+let to_string_compilation_unit (compilationUnit : compilationUnit) =
+  match compilationUnit with
+  | TypeDecl (modifiers, classDeclaration) ->
+      "(TypeDecl(\n  "
+      ^ to_string_modifiers modifiers 1
+      ^ ",\n"
+      ^ to_string_class_declaration classDeclaration 1
+      ^ ")"
 
 (* let print_compilation_unit compilation_unit =
     function
--- test/runtests.ml
+++ test/runtests.ml
@@ -50,26 +50,63 @@ let tests =
                                 [ ReturnStmt (Primary (Identifier "a")) ] ) );
                       ] ) ))
              ast );
-         (* ( "parse apex class definition with an annotation at the top"
-            >:: fun _ ->
-              let buffer = from_string "@IsTest public class AppTest {}" in
-              let ast = compilationUnit read_token buffer in
-              assert_equal
-                (TypeDecl
-                   ( [ Annotation (Identifier "IsTest"); Public ],
-                     ClassDeclaration (Identifier "AnotherTestClass", []) ))
-                ast ); *)
+         ( "parse apex class definition with an annotation at the top"
+         >:: fun _ ->
+           let buffer = from_string "@IsTest public class AppTest {}" in
+           let ast = compilationUnit read_token buffer in
+           assert_equal
+             ~printer:(fun x -> to_string_compilation_unit x)
+             (TypeDecl
+                ( [ Annotation (Identifier "IsTest"); Public ],
+                  ClassDeclaration (Identifier "AppTest", []) ))
+             ast );
+         ( "should be able to create string with num of tabs being 2"
+         >:: fun _ ->
+           let num_tabs = 2 in
+           assert_equal ~printer:(fun x -> x) "    " (tabs num_tabs) );
+         ( "should be able to create no space with tabs being 0" >:: fun _ ->
+           assert_equal ~printer:(fun x -> x) "" (tabs 0) );
          ( "tostring for the modifiers single arg" >:: fun _ ->
            let modifiers = [ Public ] in
-           assert_equal "[Public]" (to_string_modifiers modifiers) );
+           assert_equal
+             ~printer:(fun x -> x)
+             "[\n  (Public)\n]"
+             (to_string_modifiers modifiers 0) );
          ( "tostring for the modifiers multiple args" >:: fun _ ->
            let to_string_value =
-             to_string_modifiers [ Public; Annotation (Identifier "a") ]
+             to_string_modifiers [ Public; Annotation (Identifier "a") ] 0
            in
-           (*So that ; right there is ignoring the value that we are working with? *)
-           print_string to_string_value;
-           assert_equal "[Public, Annotation(Identifier(\"a\"))]"
+           assert_equal
+             ~printer:(fun x -> x)
+             "[\n\
+             \  (Public);\n\
+             \  (Annotation\n\
+             \    (Identifier\n\
+             \      (\"a\")))\n\
+              ]"
              to_string_value );
+         ( "tostring for a type decl that is a class declaration with no \
+            statement"
+         >:: fun _ ->
+           let typeDecl =
+             TypeDecl
+               ( [ Annotation (Identifier "IsTest"); Public ],
+                 ClassDeclaration (Identifier "AnotherTestClass", []) )
+           in
+           let to_string_type_decl = to_string_compilation_unit typeDecl in
+           assert_equal
+             ~printer:(fun x -> x)
+             "(TypeDecl(\n\
+             \  [\n\
+             \    (Annotation\n\
+             \      (Identifier\n\
+             \        (\"IsTest\")));\n\
+             \    (Public)\n\
+             \  ],\n\
+             \  (ClassDeclaration\n\
+             \    (Identifier\n\
+             \      (\"AnotherTestClass\")),\n\
+             \    [])))" to_string_type_decl );
        ]
 
 let _ = run_test_tt_main tests
