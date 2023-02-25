117de5ddad7d8ffa8b37cd4254e876b1868d2605
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-02-25 14:01:28 -0700

    fix: remove commented code from modifier

--- lib/modifier.ml
+++ lib/modifier.ml
@@ -44,13 +44,3 @@ let pr_modifier (ppf : Format.formatter) (modifier : modifier) : unit =
       Format.fprintf ppf
         "(Modifier@;<1 2>@[{modifier_type=\"%s\";@;<1 2>loc=%a}@]"
         (to_string modifier_type) Location.pr_location loc
-
-(* let create ?(loc = None) (identifier : ApexIdentifier.apexIdentifier) : apexType
-     =
-   { identifier; loc } *)
-
-(* let pr_apex_type (ppf : Format.formatter) (apex_type : apexType) : unit =
-   match apex_type with
-   | { identifier; loc } ->
-       Format.fprintf ppf "(ApexType@;<1 2>@[{identifier=%a;@;<1 2>loc=%a}@])"
-         ApexIdentifier.pr_identifer identifier Location.pr_location loc *)
