32a76f7af7eb89dfbe7675618721fa875658a1cd
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-02-26 23:30:13 -0700

    fix: add string literal pp test

--- lib/apexLiteral.ml
+++ lib/apexLiteral.ml
@@ -3,6 +3,7 @@ open Location
 type apexLiteral =
   | IntegerLiteral of Location.location * int
   | LongLiteral of Location.location * int
+  | StringLiteral of Location.location * string
 
 let pr_apex_literal (ppf : Format.formatter) (apex_literal : apexLiteral) : unit
     =
@@ -12,4 +13,8 @@ let pr_apex_literal (ppf : Format.formatter) (apex_literal : apexLiteral) : unit
         pr_location location
   | LongLiteral (location, long) ->
       Format.fprintf ppf "(LongLiteral@;<1 2>@[{long=%d;@;<1 2>loc=%a}@])" long
+        pr_location location
+  | StringLiteral (location, string) ->
+      Format.fprintf ppf
+        "(StringLiteral@;<1 2>@[{string=\"%s\";@;<1 2>loc=%a}@])" string
         pr_location location
--- test/apexLiteralTest.ml
+++ test/apexLiteralTest.ml
@@ -18,11 +18,21 @@ let suite =
          ( "it should be able to pretty print a long literal" >:: fun _ ->
            let buffer = Buffer.create 5 in
            let formatter = Format.formatter_of_buffer buffer in
-           let integerLiteral = LongLiteral (no_loc, 100) in
-           pr_apex_literal formatter integerLiteral;
+           let longLiteral = LongLiteral (no_loc, 100) in
+           pr_apex_literal formatter longLiteral;
            Format.pp_print_flush formatter ();
            assert_equal
              ~printer:(fun x -> x)
              "(LongLiteral\n  {long=100; loc=(Location)})"
+             (Buffer.contents buffer) );
+         ( "it should be able to pretty print a string literal" >:: fun _ ->
+           let buffer = Buffer.create 5 in
+           let formatter = Format.formatter_of_buffer buffer in
+           let stringLiteral = StringLiteral (no_loc, "abc") in
+           pr_apex_literal formatter stringLiteral;
+           Format.pp_print_flush formatter ();
+           assert_equal
+             ~printer:(fun x -> x)
+             "(StringLiteral\n  {string=\"abc\"; loc=(Location)})"
              (Buffer.contents buffer) );
        ]
