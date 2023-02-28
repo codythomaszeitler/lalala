fadf547b7c84d6e7396bf7849cb851db4ee05a1e
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-02-28 00:34:01 -0700

    fix: before formatting change

--- lib/expr.ml
+++ lib/expr.ml
@@ -1,7 +1,14 @@
-type expr = Primary of Location.location * ApexLiteral.apexLiteral
+type expr =
+  | Primary of Location.location * ApexLiteral.apexLiteral
+  | Binary of Location.location * expr * Operator.operator * expr
 
-let pr_expr (ppf : Format.formatter) (expr : expr) : unit =
+let rec pr_expr (ppf : Format.formatter) (expr : expr) : unit =
   match expr with
   | Primary (location, literal) ->
-      Format.fprintf ppf "(Primary@;<1 2>@[apexLiteral=%a;@;<1 2>loc=%a@])"
+      Format.fprintf ppf "(Primary {@[<v>@;apexLiteral=%a;@;loc=%a@;@]})"
         ApexLiteral.pr_apex_literal literal Location.pr_location location
+  | Binary (location, left, op, right) ->
+      Format.fprintf ppf
+        "(Binary{@[<v>@;@[left=%a;@]@;@[op=%a;@]@;@[right=%a;@]@;@[loc=%a@]})@]"
+        pr_expr left Operator.pr_operator op pr_expr right Location.pr_location
+        location
--- test/exprTest.ml
+++ test/exprTest.ml
@@ -2,20 +2,54 @@ open OUnit2
 open Lalala.Location
 open Lalala.Expr
 open Lalala.ApexLiteral
+open Lalala.Operator
 
 let suite =
   "Expr"
   >::: [
          ( "it should be able to pretty print an integer literal" >:: fun _ ->
+            let buffer = Buffer.create 5 in
+            let formatter = Format.formatter_of_buffer buffer in
+            let primaryExpr = Primary (no_loc, IntegerLiteral (no_loc, 100)) in
+            pr_expr formatter primaryExpr;
+            Format.pp_print_flush formatter ();
+            assert_equal
+              ~printer:(fun x -> x)
+              "(Primary {apexLiteral=(IntegerLiteral{
+              \                int=100;
+              \                loc=(Location)
+              \                });
+              \        loc=(Location)
+              \        })"
+              (Buffer.contents buffer) );
+         ( "it should be able to pretty print a binary expression" >:: fun _ ->
            let buffer = Buffer.create 5 in
            let formatter = Format.formatter_of_buffer buffer in
-           let primaryExpr = Primary (no_loc, IntegerLiteral (no_loc, 100)) in
-           pr_expr formatter primaryExpr;
-           Format.pp_print_flush formatter ();
+           let left = Primary (no_loc, IntegerLiteral (no_loc, 100)) in
+           let right = Primary (no_loc, IntegerLiteral (no_loc, 200)) in
+           let op = Add no_loc in
+           let binary = Binary (no_loc, left, op, right) in
+           pr_expr formatter binary;
+           Format.pp_print_newline formatter ();
            assert_equal
              ~printer:(fun x -> x)
-             "(Primary\n\
-             \  apexLiteral=(IntegerLiteral {int=100; loc=(Location)}); \
-              loc=(Location))"
+             "(Binary{\n\
+             \        left=(Primary {apexLiteral=(IntegerLiteral{\n\
+             \                                                   int=100;\n\
+             \                                                   loc=(Location)\n\
+             \                                                   \n\
+             \                                                   });\n\
+             \                       loc=(Location)\n\
+             \                       });\n\
+             \        op=(Add{\n\
+             \                loc=(Location)});\n\
+             \        right=(Primary {apexLiteral=(IntegerLiteral{\n\
+             \                                                    int=200;\n\
+             \                                                    loc=(Location)\n\
+             \                                                    \n\
+             \                                                    });\
+             \                        loc=(Location)\n\
+             \                        });\n\
+             \        loc=(Location)})\n"
              (Buffer.contents buffer) );
        ]
