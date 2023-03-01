024c4df5d35504d92bab3452ccac1bfad627f550
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-02-28 23:40:15 -0700

    fix: finally get all tests running

--- test/runtests.ml
+++ test/runtests.ml
@@ -7,6 +7,8 @@ open Lalala.Location
 open Lalala.ApexModifier
 open Lalala.ApexIdentifier
 open Lalala.ApexAnnotation
+open Lalala.ApexClassBodyDecl
+open Lalala.ApexMemberDecl
 
 let suite =
   "test suite for apex lexer"
@@ -22,81 +24,68 @@ let suite =
                   ApexIdentifier (no_loc, "TestClass"),
                   [] ))
              ast );
-         (* ( "parse apex class definition empty ast with different name"
-            >:: fun _ ->
-              let buffer = from_string "public class AnotherTestClass {}" in
-              let ast = compilationUnit read_token buffer in
-              assert_equal
-                (TypeDecl
-                   ( [ Public ],
-                     ClassDeclaration (Identifier "AnotherTestClass", []) ))
-                ast );
-            ( "parse apex class definition with variable declaration in it"
-            >:: fun _ ->
-              let buffer =
-                from_string
-                  "public class AnotherTestClass { public int a; public int \
-                   getA() {return a;} }"
-              in
-              let ast = compilationUnit read_token buffer in
-              assert_equal
-                (TypeDecl
-                   ( [ Public ],
-                     ClassDeclaration
-                       ( Identifier "AnotherTestClass",
-                         [
-                           ClassBodyDeclaration
-                             ( Public,
-                               FieldDeclaration
-                                 ( ApexType (Identifier "int"),
-                                   [ VariableDecl (Identifier "a") ] ) );
-                           ClassBodyDeclaration
-                             ( Public,
-                               MethodDeclaration
-                                 ( ApexType (Identifier "int"),
-                                   Identifier "getA",
-                                   [ ReturnStmt (Primary (Identifier "a")) ] ) );
-                         ] ) ))
-                ast );
-            ( "parse apex class definition with an annotation at the top"
-            >:: fun _ ->
-              let buffer = from_string "@IsTest public class AppTest {}" in
-              let ast = compilationUnit read_token buffer in
-              assert_equal
-                ~printer:(fun x -> CompilationUnit.to_string x)
-                (TypeDecl
-                   ( [ Annotation (Identifier "IsTest"); Public ],
-                     ClassDeclaration (Identifier "AppTest", []) ))
-                ast );
-            ( "should be able to create string with num of tabs being 2"
-            >:: fun _ ->
-              let num_tabs = 2 in
-              assert_equal ~printer:(fun x -> x) "    " (tabs num_tabs) );
-            ( "should be able to create no space with tabs being 0" >:: fun _ ->
-              assert_equal ~printer:(fun x -> x) "" (tabs 0) );
-            ( "tostring for a type decl that is a class declaration with no \
-               statement"
-            >:: fun _ ->
-              let typeDecl =
-                TypeDecl
-                  ( [ Annotation (Identifier "IsTest"); Public ],
-                    ClassDeclaration (Identifier "AnotherTestClass", []) )
-              in
-              let to_string_type_decl = CompilationUnit.to_string typeDecl in
-              assert_equal
-                ~printer:(fun x -> x)
-                "(TypeDecl(\n\
-                \  [\n\
-                \    (Annotation\n\
-                \      (Identifier\n\
-                \        (\"IsTest\")));\n\
-                \    (Public)\n\
-                \  ],\n\
-                \  (ClassDeclaration\n\
-                \    (Identifier\n\
-                \      (\"AnotherTestClass\")),\n\
-                \    [])))" to_string_type_decl );
-         *)
+         ( "parse apex class definition empty ast with different name"
+         >:: fun _ ->
+           let buffer = from_string "public class AnotherTestClass {}" in
+           let ast = compilationUnit read_token buffer in
+           assert_equal
+             (ApexClassDeclaration
+                ( no_loc,
+                  None,
+                  [ Public no_loc ],
+                  ApexIdentifier (no_loc, "AnotherTestClass"),
+                  [] ))
+             ast );
+         ( "parse apex class definition with no modifier" >:: fun _ ->
+           let buffer = from_string "class AnotherTestClass {}" in
+           let ast = compilationUnit read_token buffer in
+           assert_equal
+             (ApexClassDeclaration
+                ( no_loc,
+                  None,
+                  [],
+                  ApexIdentifier (no_loc, "AnotherTestClass"),
+                  [] ))
+             ast );
+         ( "parse apex class definition with variable declaration in it"
+         >:: fun _ ->
+           let buffer =
+             from_string
+               "public class AnotherTestClass { public int a; public int \
+                getA() {return a;} }"
+           in
+           let ast = compilationUnit read_token buffer in
+           assert_equal ~printer:to_string
+             (ApexClassDeclaration
+                ( no_loc,
+                  None,
+                  [ Public no_loc ],
+                  ApexIdentifier (no_loc, "AnotherTestClass"),
+                  [
+                    ApexClassBodyDeclaration
+                      ( no_loc,
+                        Public no_loc,
+                        ApexFieldDeclaration
+                          ( no_loc,
+                            ApexTypeName (no_loc, ApexIdentifier (no_loc, "int")),
+                            [
+                              ApexVariableDecl
+                                (no_loc, ApexIdentifier (no_loc, "a"));
+                            ] ) );
+                    ApexClassBodyDeclaration
+                      ( no_loc,
+                        Public no_loc,
+                        ApexMethodDeclaration
+                          ( no_loc,
+                            ApexTypeName (no_loc, ApexIdentifier (no_loc, "int")),
+                            ApexIdentifier (no_loc, "getA"),
+                            [
+                              ApexReturnStmt
+                                ( no_loc,
+                                  Id (no_loc, ApexIdentifier (no_loc, "a")) );
+                            ] ) );
+                  ] ))
+             ast );
          StmtTest.suite;
          ApexTypeNameTest.suite;
          ExprTest.suite;
